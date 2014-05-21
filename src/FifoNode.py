462ff044e44c32a4e784ac3a7f046c6ffa81f88e
vlekakis <lekakis@gmail.com>
2014-05-21 00:06:10 -0400

    Tornado workers
    making the workers play with tornado and register callbacks
    for incoming events

--- src/FifoNode.py
+++ src/FifoNode.py
@@ -1,34 +1,59 @@
 import zmq
+from zmq.eventloop import ioloop
+from zmq.eventloop import zmqstream
 from sys import exit
 from sys import argv
 import logging
 import json
 
+from time import sleep
 
+NodeIloop = None
 
-def fifoNetWorker(netName, ctrlAddress):
+logging.basicConfig(filename="process.cmd",level=logging.DEBUG)
+class FifoStats(object):
     
-    logname = netName.replace(':', '_')+'.log'
-    LOG_FILENAME = logname
-    logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
-    
-    context = zmq.Context()
-    
-    netCtrlRxSock = context.socket(zmq.SUB)
-    netCtrlRxSock.setsockopt(zmq.SUBSCRIBE, netName)
-    netCtrlRxSock.setsockopt(zmq.SUBSCRIBE, b'Exit')
-    netCtrlRxSock.connect(ctrlAddress)
-    
-    
-    poller = zmq.Poller()
-    poller.register(netCtrlRxSock, zmq.POLLIN)
-    
-    
-    while True:
-        socks = dict(poller.poll())
-        if socks.get(netCtrlRxSock) == zmq.POLLIN:
-            msg = netCtrlRxSock.recv_string()
-            logging.debug("LocalName:\t"+netName+'\t message\t'+msg )
-            if msg == 'Exit':
-                exit(1)
+    def __init__(self):
+        self.tx = {}
+        self.rx = {}
+        self.n = 0
+        self.conStatus = {}
+        
 
+class FifoNode(object):
+    
+    def __init__(self):
+        ioloop.install()
+        self.fifoStats = FifoStats()
+        self.nodeIloop = ioloop.IOLoop.instance()
+
+    def processCmd(self, stream , msg):
+    
+        if msg[0] == 'Exit':
+            logging.debug("Received exit")
+            stream.stop_on_recv()
+            self.nodeIloop.stop()
+        
+        logging.debug("Exiting process cmd")
+        return
+        
+    
+    def runFifoNetWorker(self, netName, ctrlAddress):
+    
+    
+        context = zmq.Context()
+        netCtrlRxSock = context.socket(zmq.SUB)
+        netCtrlRxSock.setsockopt(zmq.SUBSCRIBE, netName)
+        netCtrlRxSock.setsockopt(zmq.SUBSCRIBE, b'Exit')
+        netCtrlRxSock.connect(ctrlAddress)
+        streamCtrl = zmqstream.ZMQStream(netCtrlRxSock)
+    
+        streamCtrl.on_recv_stream(self.processCmd)
+        self.nodeIloop.start()
+        
+    
+    
+    
+              
+            
+    
--- src/driver.py
+++ src/driver.py
@@ -5,7 +5,8 @@ from time import sleep
 from NodeStatus import NodeStatus
 import zmq
 from multiprocessing import Process
-from FifoNode import fifoNetWorker
+from FifoNode import FifoNode
+import json
 
 Nodes = {}
 
@@ -22,9 +23,11 @@ def isLocalNode(ip):
     return False
 
 def establishLocalNode(nodeName, ctrlAddress):
-    netWorker = Process(target=fifoNetWorker, args=(nodeName, ctrlAddress))
+    fifoNode = FifoNode()
+    netWorker = Process(target=fifoNode.runFifoNetWorker, args=(nodeName, ctrlAddress))
     netWorker.start()
     print 'Node', nodeName , 'initated'
+    return netWorker
     
 
 def establishRemoteNode(node):
@@ -32,8 +35,10 @@ def establishRemoteNode(node):
 
 def initiateNodes(filename, ctrlAddress):
     
+    workers = []
     fp = open(filename)
     for host in fp:
+        print host
         host = host.rstrip("\n")
         if ':' not in host:
             print 'Not valid node file, the program will exit'
@@ -47,17 +52,28 @@ def initiateNodes(filename, ctrlAddress):
         Nodes[host]['ip'] = node[0]
         Nodes[host]['port'] = node[1]
         Nodes[host]['status'] = NodeStatus.DRIVER_PARSED
-        Nodes[host]['name'] = host
+        Nodes[host]['name'] = "tcp://"+host
   
         if isLocalNode(Nodes[host]['ip']) == True:
             res = establishLocalNode(host, ctrlAddress)
-        
+            Nodes[host]['status'] = NodeStatus.DRIVER_INITIALIZED
+            workers.append(res)
         else:
             print 'Remote node, not supported yet'
             
-        if res == 0:
-            Nodes[host]['status'] = NodeStatus.DRIVER_INITIALIZED
+    return workers
+            
+        
   
+def testServerNodes(context):
+    sock = context.socket(zmq.REQ)
+    for k in Nodes.keys():
+        sock.connect(Nodes[k]['name'])
+        sock.send_string("Test")
+        print "AFTER SEND"
+        msg = sock.recv_string()
+        print 'Node' , k, 'replied' , msg
+        
   
         
 def main():
@@ -79,7 +95,7 @@ def main():
     
     print 'Initiating nodes from file....', args.nodes
     ctrlAddress = 'tcp://127.0.0.1:5558'
-    initiateNodes(args.nodes, ctrlAddress)
+    w = initiateNodes(args.nodes, ctrlAddress)
     
     
     context = zmq.Context()
@@ -87,18 +103,10 @@ def main():
     ctrlSock.bind(ctrlAddress)
     print 'Test'
     sleep(1)
-    for msg in xrange(100):
-        address = ''
-        if msg % 3 == 0:
-            address = 'localhost:5000'
-        elif msg % 3 == 1:
-            address = 'localhost:5001'
-        if msg % 3 == 2:
-            address = 'localhost:5002'
-        
-        msg = address+'\t'+str(msg)
-        ctrlSock.send_string(msg)
-    
+     
+#     serverPort = json.dumps({'server':'5000'})
+#     ctrlSock.send_multipart(['localhost:5000', serverPort ])
+    #testServerNodes(context)    
     ctrlSock.send_string('Exit')
     
     exit(1)
